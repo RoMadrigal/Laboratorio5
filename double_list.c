12c7273f48a2045415af25f2bcb0f280c5ea1b25
Rodrigo M <rodrigo@Ubuntu.myguest.virtualbox.org>
2024-06-03 21:28:38 -0600

    Se inserta un nodo al final de la lista y en una posicion determinada

--- double_list.c
+++ double_list.c
@@ -26,3 +26,36 @@ void insertar_en_inicio(DoubleList* list, int data) {
 }
 
 
+void insertar_en_final(DoubleList* list, int data) {
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    new_node->data = data;
+    new_node->next = NULL;
+    new_node->prev = list->tail;
+    if (list->tail != NULL) {
+        list->tail->next = new_node;
+    }
+    list->tail = new_node;
+    if (list->head == NULL) {
+        list->head = new_node;
+    }
+}
+
+
+void insertar_en_posicion(DoubleList* list, int data, int posicion) {
+    if (position == 0) {
+        insert_at_beginning(list, data);
+        return;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
