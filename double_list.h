fda75715e4e3b14c6e3a621fd0bb3ac92bd2fb62
Rodrigo M <rodrigo@Ubuntu.myguest.virtualbox.org>
2024-06-03 20:02:41 -0600

    Se declaran las funciones a utilizar

--- double_list.h
+++ double_list.h
@@ -15,3 +15,17 @@ typedef struct DoubleList {
     Node* tail;
 } DoubleList;
 
+
+DoubleList* crear_lista();
+void insertar_en_inicio(DoubleList* list, int data);
+void insertar_en_final(DoubleList* list, int data);
+void insertar_en_posicion(DoubleList* list, int data, int posicion);
+void eliminar_nodo(DoubleList* list, int data);
+Node* buscar(DoubleList* list, int data);
+void imprimir_lista_delante(DoubleList* list);
+void imprimir_lista_atras(DoubleList* list);
+void liberar_lista(DoubleList* list);
+
+
+#endif
+
